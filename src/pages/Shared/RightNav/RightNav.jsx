dbc9e8af1b4122610fb81c02a13ace3a68cf2ed7
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-05-01 23:39:25 +0600

    added Qzone

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
     return (
         <div>
-            <h2>This is right Nav</h2>
+            <h4>Login With</h4>
+            <div class="d-flex flex-column my-3">
+                <Button className='mb-2' variant="outline-primary"><FaGoogle /> Login with Google</Button>
+                <Button variant="outline-secondary"><FaGithub /> Login with Github</Button>
+            </div>
+            <div>
+                <h4>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook />Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter />Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram/>Instgram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
